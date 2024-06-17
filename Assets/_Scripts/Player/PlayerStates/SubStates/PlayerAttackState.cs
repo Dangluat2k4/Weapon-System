ed8ea5fd444ca65982db3a752c5da1fc79349eed
Dang Luat <131101167+Dangluat2k4@users.noreply.github.com>
2024-06-17 16:37:18 +0700

    update

---
+++ Assets/_Scripts/Player/PlayerStates/SubStates/PlayerAttackState.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackState : PlayerAbilityState
+{
+    private Weapon Weapon;
+    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName, Weapon _weapon)
+        : base(player, stateMachine, playerData, animBoolName)
+    {
+        this.Weapon = _weapon;
+
+    }
+    public override void Enter()
+    {
+        base.Enter();
+        Weapon.Enter();
+    }
+}
